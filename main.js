4cfcd00801a8cd0daa2a748fbce767c903619dd2
zhaozhao <1350213347@qq.com>
2022-03-28 12:41:14 +0800

    home

--- main.js
+++ main.js
@@ -3,16 +3,42 @@
 import Vue from 'vue'
 import App from './App'
 
+import { $http } from '@escook/request-miniprogram'
+
+uni.$http = $http
+
+$http.baseUrl = 'https://www.uinav.com'
+
+
+//请求拦截器
+$http.beforeRequest = function(options){
+	uni.showLoading({
+		title:"数据加载中..."
+	})
+}
+//响应拦截器
+$http.afterRequest = function(options){
+	uni.hideLoading()
+}
+
+uni.$showMsg = function(titlec='数据请求失败!',duration=1500){
+	uni.showToast({
+		title,
+		duration,
+		icon:'none'
+	})
+},
+
 Vue.config.productionTip = false
 
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App
+    ...App 
 })
 app.$mount()
-// #endif
-
+// #endif 
+  
 // #ifdef VUE3
 import { createSSRApp } from 'vue'
 import App from './App.vue'
